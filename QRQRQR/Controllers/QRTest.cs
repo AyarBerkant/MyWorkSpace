970475e8d18e92019627a9726a1f45efc70786bb
Berkant AYAR <berkant.ayar@glomil.com>
2022-09-06 17:36:49 +0300

    Add project files.

---
+++ QRQRQR/Controllers/QRTest.cs
@@ -0,0 +1,32 @@
+﻿using System.Drawing;
+using System.IO;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using QRCoder;
+
+namespace QRQRQR.Controllers
+{
+    [Route("/[controller]")]
+    [ApiController]
+    public class QRTest : Controller
+    {
+        public byte[] Imagetoarray(System.Drawing.Image imagein)
+        {
+            MemoryStream ms = new MemoryStream();
+            imagein.Save(ms, System.Drawing.Imaging.ImageFormat.Jpeg);
+            return ms.ToArray();
+        }
+
+        [HttpPost("[action]xx")]
+        public async Task<IActionResult> GenerateQRCode(string qrCodeText)
+        {
+            QRCodeGenerator _qrCode = new QRCodeGenerator();
+            QRCodeData _qrCodeData = _qrCode.CreateQrCode(qrCodeText, QRCodeGenerator.ECCLevel.Q);
+            QRCode qrCode = new QRCode(_qrCodeData);
+            Image qrCodeImage = qrCode.GetGraphic(20);
+
+            var bytes = Imagetoarray(qrCodeImage);
+            return File(bytes, "image/bmp");
+        }
+    }
+}
---
+++ TestQRGenerator/Controllers/GeneratorTestController.cs
@@ -0,0 +1,39 @@
+﻿using System.Drawing;
+using System.Drawing.Imaging;
+using System.Reflection.Metadata.Ecma335;
+using System.Security.Cryptography;
+using QRCoder;
+using Microsoft.AspNetCore.Mvc;
+
+
+namespace TestQRGenerator.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class GeneratorTestController : Controller
+    {
+        public class MyFirstObj
+        {
+            public string Name { get; set; }
+            public string Surname { get; set; }
+        }
+        private byte[] Imagetoarray(System.Drawing.Image imagein)
+        {
+            MemoryStream ms = new MemoryStream();
+            imagein.Save(ms,System.Drawing.Imaging.ImageFormat.Jpeg);
+            return ms.ToArray();
+        }
+
+        [HttpPost("[action]")]
+        public async Task<ActionResult> GenerateQRCode([FromForm] MyFirstObj obj)
+        {
+            QRCodeGenerator _qrCode = new QRCodeGenerator();
+            QRCodeData _qrCodeData = _qrCode.CreateQrCode($"{obj.Name} {obj.Surname}", QRCodeGenerator.ECCLevel.Q);
+            QRCode qrCode = new QRCode(_qrCodeData);
+            Image qrCodeImage = qrCode.GetGraphic(20);
+
+            var bytes = Imagetoarray(qrCodeImage);
+            return File(bytes, "image/bmp");
+        }
+    }
+}
